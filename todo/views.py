6bf45db9c76cb687bcb0569fa1e5624102b54fc7
Anastasiia Ünal <velyka.nastya@icloud.com>
2024-02-02 22:30:49 +0100

    add new todo to api

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import permissions, viewsets
 from .serializers import TodoSerializer
 from .models import Todo
+from django.core import serializers
 
 # Create your views here.
 class TodoViewSet(viewsets.ModelViewSet):
@@ -11,3 +13,12 @@ class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all().order_by('-created_at')
     serializer_class = TodoSerializer
     permission_classes = [] # [permissions.IsAuthenticated] => just for authentificated users
+    
+    
+    def create(self, request):
+        todo = Todo.objects.create(title=request.data.get('title', ''),
+                                   description=request.data.get('description', ''),
+                                   user=request.user
+                                   )
+        serialized_obj = serializers.serialize('json', [todo])
+        return HttpResponse(serialized_obj, content_type='application/json')
